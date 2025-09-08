e31df43992f2706e4ec84e5627c49e4c6e9ba011
SaeZygaena <hugo.altiery@hotmail.fr>
2025-09-08 13:21:04 +0400

    Push scene rythme

--- GAME JAM/Assets/Scripts/ButtonManager.cs
+++ GAME JAM/Assets/Scripts/ButtonManager.cs
@@ -16,9 +16,8 @@ public class ButtonManager : MonoBehaviour
 
     public static event Action<string> SignalText;
 
-    private void Start()
+    void LateUpdate()
     {
-        
         buttonCollider = GetComponent<BoxCollider2D>();
         Bounds b = buttonCollider.bounds;
     }
@@ -36,30 +35,6 @@ public class ButtonManager : MonoBehaviour
     {
         if (other.CompareTag("Note"))
         {
-            // Récupère les bounds
-            Bounds a = other.gameObject.GetComponent<BoxCollider2D>().bounds;
-            Debug.Log("a =" + a);
-            //Bounds b = buttonCollider.bounds;
-            Debug.Log("b =" + b);
-
-
-            // Calcule l’intersection (largeur & hauteur)
-            float overlapX = Mathf.Max(0, Mathf.Min(a.max.x, b.max.x) - Mathf.Max(a.min.x, b.min.x));
-            float overlapY = Mathf.Max(0, Mathf.Min(a.max.y, b.max.y) - Mathf.Max(a.min.y, b.min.y));
-
-            // Surface de l’intersection
-            float overlapArea = overlapX * overlapY;
-
-            // Surfaces des deux colliders
-            float areaA = a.size.x * a.size.y;
-            float areaB = b.size.x * b.size.y;
-
-            // Pourcentage d’overlap par rapport au plus petit collider
-            float overlapPercent = overlapArea / Mathf.Min(areaA, areaB);
-
-            Debug.Log(overlapPercent);
-            SignalText?.Invoke(overlapPercent.ToString());
-
 
             if (!other.GetComponent<NoteMovement>().isValided)
             {
@@ -80,6 +55,7 @@ public class ButtonManager : MonoBehaviour
     private void ShortNoteActivate()
     {
         tempoNote.isValided = true;
+        SignalText?.Invoke("Success!");
         Destroy(tempoNoteObject);
     }
 
