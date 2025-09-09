f2cbcc19994626b8b3c17e868a93c6841e452103
SaeZygaena <hugo.altiery@hotmail.fr>
2025-09-09 15:48:45 +0400

    commit Day 2

--- GAME JAM/Assets/Scripts/RythmeGameplay/ButtonManager.cs
+++ GAME JAM/Assets/Scripts/RythmeGameplay/ButtonManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Interactions;
 using System;
+using System.Data.Common;
 
 public class ButtonManager : MonoBehaviour
 {
@@ -17,6 +18,8 @@ public class ButtonManager : MonoBehaviour
     public static event Action<string> SignalText;
 
     public bool missValue = false;
+
+    public int id;
 
     private void Start()
     {
@@ -57,7 +60,7 @@ public class ButtonManager : MonoBehaviour
 
     public void OnNote(InputAction.CallbackContext context)
     {
-        Debug.Log("Je suis ici dans le OnNote");
+        Debug.Log("Je suis ici dans le OnNote = " + id);
         if ((canActivateNote || missValue) && context.interaction is PressInteraction && context.phase == InputActionPhase.Started)
         {
             if (tempoNote.type == DataNote.NoteType.SimpleNote)
@@ -70,9 +73,9 @@ public class ButtonManager : MonoBehaviour
     private void ShortNoteActivate()
     {
         tempoNote.isValided = true;
-        if (!missValue)
+        if (canActivateNote)
             SignalText?.Invoke("Perfect!");
-        else if (missValue)
+        else
             SignalText?.Invoke("Bad!");
         Debug.Log("missValue = " + missValue);
         missValue = false;
